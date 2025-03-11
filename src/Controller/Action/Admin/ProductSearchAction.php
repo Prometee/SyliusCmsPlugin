900ece04006c983fbe52135bed8526a7e40aab73
Francis Hilaire <Prometee@users.noreply.github.com>
2025-03-11 20:18:26 +0100

    Remove fos_rest usage and @todo replace with route using grid config

--- src/Controller/Action/Admin/ProductSearchAction.php
+++ src/Controller/Action/Admin/ProductSearchAction.php
@@ -10,8 +10,6 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusCmsPlugin\Controller\Action\Admin;
 
-use FOS\RestBundle\View\View;
-use FOS\RestBundle\View\ViewHandler;
 use Sylius\Component\Core\Repository\ProductRepositoryInterface;
 use Sylius\Component\Locale\Context\LocaleContextInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,18 +20,14 @@ final class ProductSearchAction
     public function __construct(
         private ProductRepositoryInterface $productRepository,
         private LocaleContextInterface $localeContext,
-        private ViewHandler $viewHandler,
     ) {
     }
 
     public function __invoke(Request $request): Response
     {
         $product = $this->productRepository->findByNamePart($request->get('phrase', ''), $this->localeContext->getLocaleCode());
-        $view = View::create($product);
 
-        $this->viewHandler->setExclusionStrategyGroups(['Autocomplete']);
-        $view->getContext()->enableMaxDepth();
-
-        return $this->viewHandler->handle($view);
+        // @todo remove this service and use route with grid configuration instead
+        return new Response($product);
     }
 }
--- src/Controller/Action/Admin/TaxonSearchAction.php
+++ src/Controller/Action/Admin/TaxonSearchAction.php
@@ -10,8 +10,6 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusCmsPlugin\Controller\Action\Admin;
 
-use FOS\RestBundle\View\View;
-use FOS\RestBundle\View\ViewHandler;
 use Sylius\Component\Locale\Context\LocaleContextInterface;
 use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,18 +20,14 @@ final class TaxonSearchAction
     public function __construct(
         private TaxonRepositoryInterface $taxonRepository,
         private LocaleContextInterface $localeContext,
-        private ViewHandler $viewHandler,
     ) {
     }
 
     public function __invoke(Request $request): Response
     {
         $taxon = $this->taxonRepository->findByNamePart($request->get('phrase', ''), $this->localeContext->getLocaleCode());
-        $view = View::create($taxon);
 
-        $this->viewHandler->setExclusionStrategyGroups(['Autocomplete']);
-        $view->getContext()->enableMaxDepth();
-
-        return $this->viewHandler->handle($view);
+        // @todo remove this service and use route with grid configuration instead
+        return new Response($taxon);
     }
 }
